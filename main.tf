9409b6432ec01412190e8e9364b11b562905dd84
cagncvs <g.carol216@gmail.com>
2026-03-27 11:27:06 +0000

    IaC com terraform e ansible

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = "carol-goncalves"
+  #user_data = <<-EOF
+                  #!/bin/bash
+                  #cd /home/ubuntu
+                  #echo "<h1>Feito com Terraform</h1>" > index.html
+                  #nohup busybox httpd -f -p 8080 &
+                  #EOF
+  user_data_replace_on_change = true
+  tags = {
+    Name = "Teste aws"
+  }
+}
+
